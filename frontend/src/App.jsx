60351c1bd1b510c631574c9bba41128352430603
tanishqkumar2003 <tanishqpayla@gmail.com>
2024-09-18 10:14:33 +0530

    Components Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import {Signup} from "./pages/Signup"
+import {Signin} from "./pages/Signin"
+import {SendMoney} from "./pages/SendMoney"
+import {Dashboard} from "./pages/Dashboard"
 
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+            <Route path="/signup" element={Signup} />
+            <Route path="/signin" element={Signin} />
+            <Route path="/send" element={SendMoney} />
+            <Route path="/dashboard" element={Dashboard} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 

4aa1e3e2cced719810f7da8c1e96fc44364ce067
tanishqkumar2003 <tanishqpayla@gmail.com>
2024-09-19 23:19:08 +0530

    minor change

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Appbar } from './components/AppBar.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
   
     <App />
    
